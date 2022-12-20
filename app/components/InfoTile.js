cf4d77c9f51fdcce7150608d4879713003fa68c1
Fabian Kraus <fabian.kraus1994@gmail.com>
2022-12-20 20:11:06 +0100

    InfoTile Anpassungen
    Layout von InfoTile verändert; push um am PC weiterarbeiten zu können

--- app/components/InfoTile.js
+++ app/components/InfoTile.js
@@ -3,6 +3,7 @@ import { decode } from "html-entities";
 import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
 
 import colors from "../config/colors";
+import Icon from "../components/Icon";
 
 function InfoTile(props) {
   if (props.Closed == 0) {
@@ -10,11 +11,14 @@ function InfoTile(props) {
     return (
       <TouchableOpacity onPress={() => console.log(props.ID)}>
         <View style={styles.container}>
-          <Text>{props.ID}</Text>
-          <Text>{decode(props.Name)}</Text>
-          <Text>
-            Frei: {props.Available}/{props.Total}
-          </Text>
+          <Text style={styles.id}>{props.ID}</Text>
+          <View style={styles.info}>
+            <Text>{decode(props.Name)}</Text>
+            <Text>
+              Frei: {props.Available}/{props.Total}
+            </Text>
+          </View>
+          <Icon name="minus"/>
           <Text>{props.Trend}</Text>
         </View>
       </TouchableOpacity>
@@ -27,8 +31,17 @@ const styles = StyleSheet.create({
     height: 100,
     backgroundColor: colors.primary,
     marginVertical: 5,
-    padding: 5,
+    paddingHorizontal: 10,
     borderRadius: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  id:{
+    fontSize: 60,
+  },
+  info:{
+    fontSize: 40,
   },
 });
 
